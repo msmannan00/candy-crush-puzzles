03be388722d4e8775e7a66cde0e3a171be8c6ab2
msmannan00 <msmannan00@protonmail.com>
2023-06-15 12:52:19 +0500

    bug fixes
    bug fixes

--- Assets/Scripts/Gameplay/gamemanager.cs
+++ Assets/Scripts/Gameplay/gamemanager.cs
@@ -5,18 +5,22 @@ using TMPro;
 using System.Collections;
 using UnityEngine.Windows.Speech;
 using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 public class gamemanager : MonoBehaviour
 {
     public Light2D light2D;
     public TextMeshProUGUI score;
+    public TextMeshProUGUI remainingTime;
     public GameObject speak;
     public GameObject faillPopup;
     public Light2D[] statusLight;
 
-    protected KeywordRecognizer recognizer;
+    private DictationRecognizer recognizer;
     public ConfidenceLevel confidence = ConfidenceLevel.Medium;
 
+    private Coroutine countDownCoroutine = null;
     private bool sessionStarted = false;
     private List<string> sessionResult;
     private List<List<string>> currentLevelLightCombination;
@@ -26,14 +30,23 @@ public class gamemanager : MonoBehaviour
 
     private void Start()
     {
-        recognizer = new KeywordRecognizer(availableColorNames, confidence);
-        recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
+        // Create an instance of the DictationRecognizer.
+        recognizer = new DictationRecognizer();
+
+        // Register the event handlers.
+        recognizer.AutoSilenceTimeoutSeconds = 20f;
+        recognizer.InitialSilenceTimeoutSeconds = 20f;
+        recognizer.DictationHypothesis += OnDictationHypothesis;
+        recognizer.DictationResult += OnDictationResult;
+        recognizer.DictationComplete += OnDictationComplete;
+        recognizer.DictationError += OnDictationError;
+        // Start the dictation recognizer.
         recognizer.Start();
 
         currentLevelLightCombination = new List<List<string>>();
         sessionResult = new List<string>();
 
-        currentLevelLightCombination.AddRange(GeneratePermutations(new List<string> { "red", "green", "blue", "white" }, 3, 5));
+        currentLevelLightCombination.AddRange(GeneratePermutations(new List<string> { "red", "green", "blue", "white" }, 3, 33));
         currentLevelLightCombination.AddRange(GeneratePermutations(new List<string> { "red", "green", "blue", "white" }, 5, 33));
         currentLevelLightCombination.AddRange(GeneratePermutations(new List<string> { "red", "green", "blue", "white" }, 7, 33));
 
@@ -67,7 +80,7 @@ public class gamemanager : MonoBehaviour
             }
             else
             {
-                yield return new WaitForSeconds((1 / (currentCombinationIndex*2)));
+                yield return new WaitForSeconds((1 / (currentCombinationIndex * 2)));
             }
         }
 
@@ -87,6 +100,26 @@ public class gamemanager : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         speak.SetActive(true);
         sessionStarted = true;
+        countDownCoroutine = StartCoroutine(Countdown());
+    }
+
+
+    private IEnumerator Countdown()
+    {
+        int countdownValue = ((currentCombinationIndex / 33) + 1) * 5;
+
+        while (countdownValue > 0)
+        {
+            remainingTime.text = countdownValue + "sec";
+            yield return new WaitForSeconds(1f);
+            countdownValue--;
+        }
+
+        faillPopup.SetActive(true);
+        speak.SetActive(false);
+        sessionStarted = false;
+        yield return new WaitForSeconds(2f);
+        remainingTime.text = countdownValue + "sec";
     }
 
     private IEnumerator FadeColor(Color startColor, Color targetColor, float duration)
@@ -172,7 +205,7 @@ public class gamemanager : MonoBehaviour
             case "white":
                 return Color.white;
             default:
-                return Color.white;
+                return Color.magenta;
         }
     }
 
@@ -182,44 +215,85 @@ public class gamemanager : MonoBehaviour
         SceneManager.LoadScene("gameplay");
     }
 
-    private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    private void OnDictationHypothesis(string text)
     {
+    }
+
+    private void OnDictationResult(string text, ConfidenceLevel confidence)
+    {
+        Debug.Log("Result: " + text);
+        string[] words = Regex.Split(text.ToLower(), @"\W+");
+
         if (sessionStarted)
         {
-            int currentIndex = sessionResult.Count;
-            string response = args.text;
-            sessionResult.Add(response);
-
-            if(sessionResult[currentIndex] == currentLevelLightCombination[currentCombinationIndex - 1][currentIndex])
+            sessionResult = words.ToList();
+            bool equal = sessionResult.SequenceEqual(currentLevelLightCombination[currentCombinationIndex - 1]);
+            if (sessionResult.Count == currentLevelLightCombination[currentCombinationIndex - 1].Count && equal)
             {
-                statusLight[sessionResult.Count-1].color = GetColorFromString(sessionResult[currentIndex]);
-                if (sessionResult.Count == currentLevelLightCombination[currentCombinationIndex - 1].Count)
+                for (int i = 0; i < statusLight.Length; i++)
                 {
+                    statusLight[i].color = Color.clear;
+                }
 
-                    for (int i = 0; i < sessionResult.Count; i++)
-                    {
-                        statusLight[i].color = Color.clear;
-                    }
+                if (currentCombinationIndex >= 99)
+                {
+                    currentCombinationIndex = 0;
+                }
 
-                    if (currentCombinationIndex >= 0)
-                    {
-                        currentCombinationIndex = 0;
-                    }
-
-                    StartCoroutine(SwitchColorsCoroutine());
-                    speak.SetActive(false);
-                    sessionStarted = false;
-                    sessionResult.Clear();
-                    score.text = "Level " + currentCombinationIndex;
+                StartCoroutine(SwitchColorsCoroutine());
+                speak.SetActive(false);
+                sessionStarted = false;
+                sessionResult.Clear();
+                score.text = "Level " + currentCombinationIndex;
+                if (countDownCoroutine != null)
+                {
+                    StopCoroutine(countDownCoroutine);
                 }
             }
             else
             {
-                statusLight[sessionResult.Count - 1].color = GetColorFromString(sessionResult[currentIndex]);
+                if (words.Length < 7)
+                {
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        statusLight[i].color = GetColorFromString(words[i]);
+                    }
+                }
+
                 faillPopup.SetActive(true);
                 speak.SetActive(false);
+                if (countDownCoroutine != null)
+                {
+                    StopCoroutine(countDownCoroutine);
+                }
             }
-            Debug.Log(args.text);
+        }
+
+    }
+
+    private void OnDictationComplete(DictationCompletionCause cause)
+    {
+        Debug.Log("Dictation complete. Cause: " + cause.ToString());
+
+        recognizer.Stop();
+        recognizer.Start();
+    }
+
+    private void OnDictationError(string error, int hresult)
+    {
+        Debug.LogError("Dictation error: " + error + ", HRESULT: " + hresult);
+
+        recognizer.Stop();
+        recognizer.Start();
+    }
+
+    private void OnDestroy()
+    {
+        // Stop and dispose of the dictation recognizer when it's no longer needed.
+        if (recognizer != null)
+        {
+            recognizer.Stop();
+            recognizer.Dispose();
         }
     }
 }
