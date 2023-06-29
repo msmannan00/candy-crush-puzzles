4a6dea1b9991e5ab05f2aad6a59c9762be2b02ee
msmannan00 <msmannan00@protonmail.com>
2023-06-30 03:11:03 +0500

    Bug Fixes

--- Assets/GooglePlayGames/GameInfo.cs
+++ Assets/GooglePlayGames/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = "680874118210"; // Filled in automatically
+        public const string ApplicationId = "1015933271071"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = "680874118210-i4m5esr0gi40dfot05d96feuuigeuiag.apps.googleusercontent.com"; // Filled in automatically
+        public const string WebClientId = "1015933271071-isimkuknjbfdtvqrg1g2m8a4gs28id07.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
